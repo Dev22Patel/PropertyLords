7b19234179257899d3e8bc370be7ca054392a1aa
Dev22Patel <156922885+Dev22Patel@users.noreply.github.com>
2024-08-29 00:21:15 +0530

    first commit

---
+++ realestate/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:realestate/features/app/splash_screen/splash_screen.dart';
+import 'package:realestate/features/user_auth/presentation/pages/home_page.dart';
+import 'package:realestate/features/user_auth/presentation/pages/login_page.dart';
+import 'package:realestate/features/user_auth/presentation/pages/sign_up_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+import 'dart:io' as io;
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  io.Platform.isAndroid?
+  await Firebase.initializeApp(
+    options:const FirebaseOptions(
+      apiKey: "AIzaSyBDmZjJ2SDQatkr3vy0kcmy9CtdrHY8-lE",
+      appId: "1:508354716022:android:dedad05a6f9cb04bc89a57",
+      messagingSenderId: "508354716022",
+      projectId: "flutter-firebase-3c0d2",
+    ),
+  )
+  :
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+     routes: {
+        '/': (context) => SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
+        ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
+  }
+}
